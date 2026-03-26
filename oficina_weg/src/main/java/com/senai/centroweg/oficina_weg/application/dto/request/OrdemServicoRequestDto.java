1827bcb9822e31c53c10ab887a037268934d02fb
vic376 <nicoladellivick@gmail.com>
2026-03-26 16:42:44 -0300

    Criacao de DTO OS - exceto mapper

--- oficina_weg/src/main/java/com/senai/centroweg/oficina_weg/application/dto/request/OrdemServicoRequestDto.java
+++ oficina_weg/src/main/java/com/senai/centroweg/oficina_weg/application/dto/request/OrdemServicoRequestDto.java
@@ -1,4 +1,13 @@
 package com.senai.centroweg.oficina_weg.application.dto.request;
 
-public record OrdemServicoRequestDto() {
+import java.util.List;
+
+public record OrdemServicoRequestDto(
+         String equipamento,
+         String defeito,
+         String materiaisQuantidade,
+         String laudoTecnico,
+         Long idProfessorResponsavel,
+         List<Long> alunosIds
+) {
 }
--- oficina_weg/src/main/java/com/senai/centroweg/oficina_weg/application/dto/response/OrdemServicoResponseDto.java
+++ oficina_weg/src/main/java/com/senai/centroweg/oficina_weg/application/dto/response/OrdemServicoResponseDto.java
@@ -1,4 +1,17 @@
 package com.senai.centroweg.oficina_weg.application.dto.response;
 
-public class OrdemServicoResponseDto {
+import com.senai.centroweg.oficina_weg.domain.model.StatusOrdemServico;
+
+import java.util.List;
+
+public record OrdemServicoResponseDto(
+         Long id,
+         String equipamento,
+         String defeito,
+         String materiaisQuantidade,
+         String laudoTecnico,
+         StatusOrdemServico status,
+         Long idProfessorResponsavel,
+         List<AlunoResponseDto> alunosResponsaveis
+) {
 }
