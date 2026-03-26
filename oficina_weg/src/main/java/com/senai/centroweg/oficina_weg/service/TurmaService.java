0e76e16736b6b46bd98fc7d43d7655660ad06961
vic376 <nicoladellivick@gmail.com>
2026-03-26 15:51:32 -0300

    Implementacao do service de turma

--- oficina_weg/src/main/java/com/senai/centroweg/oficina_weg/service/TurmaService.java
+++ oficina_weg/src/main/java/com/senai/centroweg/oficina_weg/service/TurmaService.java
@@ -2,12 +2,28 @@ package com.senai.centroweg.oficina_weg.service;
 
 import com.senai.centroweg.oficina_weg.domain.interfaces.IOrdemServicoRepository;
 import com.senai.centroweg.oficina_weg.domain.interfaces.ITurmaRepository;
+import com.senai.centroweg.oficina_weg.domain.model.Turma;
+
+import java.util.List;
+import java.util.Optional;
 
 public class TurmaService {
 
-    private final ITurmaRepository repositoy;
+    private final ITurmaRepository repository;
 
     public TurmaService(ITurmaRepository repositoy) {
-        this.repositoy = repositoy;
+        this.repository = repositoy;
+    }
+
+    public Turma criarTurma (Turma turma){
+        return repository.save(turma);
+    }
+
+    public Optional<Turma> buscarPorId( Long id){
+        return repository.findById(id);
+    }
+
+    public List<Turma> listarTurmas (){
+        return repository.findAll();
     }
 }
